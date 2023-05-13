2a4876a9cb3b757e14dda990c83fc7266602a50b
Nicholas Lancaster <NickRobLancaster@gmail.com>
2023-05-13 02:14:57 -0700

    asdf

--- src/components/WebsiteSPA.vue
+++ src/components/WebsiteSPA.vue
@@ -344,7 +344,7 @@ const copyText = (textToCopy) => {
 
       <div v-show="page === 2" class="flex-1">
         <div
-          class="relative flex flex-col justify-center items-center gap-10 w-full min-h-full bg-gradient-to-br from-indigo-500 to-sky-500 p-3 py-20"
+          class="relative flex flex-col justify-center items-center gap-10 w-full min-h-full bg-gradient-to-br from-rose-500 to-yellow-500 p-3 py-20"
         >
           <div
             :class="
